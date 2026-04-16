7fe9225d1ad1fd760a6e7bac83b135983354a7bd
jcarrasqueira <joanarcarrasqueira02@gmail.com>
2026-04-16 02:44:16 +0100

    adjustments makepayment

--- decentralized_contract.sol
+++ decentralized_contract.sol
@@ -5,13 +5,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DecentralizedFinance is ERC20 {
     address private owner;
-    uint256 private nextLoanID = 1;
-    uint256 private balance; // balance of contract in Wei
+    uint256 private nextLoanID = 1; // serves as a counter for loan ids
+    uint256 private balance; // balance of contract in wei
     
     uint256 public paymentCycle; // payment cycle duration  == periocity
-    uint256 public interest; // interest rate -> 10 = 10%
+    uint256 public interest; // interest rate
     uint256 public termination; // termination fee
-    uint256 public maxLoanDuration = 10; // in cycles //later add a setter for contract owner
+    uint256 public maxLoanDuration = 10; // in cycles 
     uint256 public dexSwapRate; // how many Wei a DEX costs
     
     struct Loan {
@@ -31,7 +31,7 @@ contract DecentralizedFinance is ERC20 {
 
     constructor(uint256 _dexSwapRate, uint256 _paymentCycle, uint256 _interest, uint256 _termination) ERC20("DEX", "DEX") {
         owner = msg.sender;
-
+        
         dexSwapRate = _dexSwapRate;
         paymentCycle = _paymentCycle;
         interest = _interest;
@@ -40,7 +40,7 @@ contract DecentralizedFinance is ERC20 {
         _mint(address(this), 10**18);
     }
 
-    modifier onlyOwner() {
+    modifier onlyOwner() { // helper to limit access to owner fuctions (checkLoan and getBalance)
         require(msg.sender == owner, "Only contract owner can call this function.");
         _;
     }
@@ -90,13 +90,13 @@ contract DecentralizedFinance is ERC20 {
     }
 
     function loan(uint256 dexAmount, uint256 deadline) external returns(uint256 loanID){
-        require(dexAmount > 0, "No DEX sent"); // checks collateral
+        require(dexAmount > 0, "No DEX sent"); // checks collateral dex
         require(balanceOf(msg.sender) >= dexAmount , "Client doesn't own enough DEX for this loan."); // checks user balance
         require(deadline > 0 && deadline <= maxLoanDuration, "Invalid deadline"); // checks for deadline
 
         // computes and validates collateral amount (eth)
         uint256 collateralEth = dexAmount * dexSwapRate; 
-        uint256 loanAmount = collateralEth / 2; // only 50% can be borrowed //change to percentage since int division
+        uint256 loanAmount = collateralEth / 2; // only 50% can be borrowed 
         require(address(this).balance >= loanAmount, "Contract doesn't have enough ETH"); // checks if amount in contract is enough for loan
         
         // sends collateral to contract
@@ -148,19 +148,19 @@ contract DecentralizedFinance is ERC20 {
             _transfer(address(this), currentLoan.borrower, currentLoan.collateral); // send collateral back
             emit loanFinished(currentLoan.borrower, currentLoan.amount);
         } 
-        else { // next deadline
+        else { // next payment cycle
             nextPayment[loanID] += paymentCycle;
         }
     }
 
-    function terminateLoan(uint256 loanID) external payable {
+    function terminateLoan(uint256 loanID) external payable { 
         Loan storage currentLoan = loans[loanID];
         require(currentLoan.borrower != address(0), "Loan does not exist");
         require(active[loanID], "Loan is not active");
         require(msg.sender == currentLoan.borrower, "Only borrower can terminate");
-        //require(block.timestamp <= nextPayment[loanID], "Payment deadline passed");
 
-        uint256 totalDue = currentLoan.amount + termination;
+        // in order to terminate user must pay the value of termination fee
+        uint256 totalDue = currentLoan.amount + termination; 
         require(msg.value >= totalDue, "Insufficient repayment");
         
         balance += totalDue;
@@ -169,7 +169,7 @@ contract DecentralizedFinance is ERC20 {
         // Return the DEX collateral to the user
         _transfer(address(this), msg.sender, currentLoan.collateral);
 
-        uint256 excess = msg.value - totalDue;
+        uint256 excess = msg.value - totalDue; // in case value sent is superior to value needed returns the rest
         if (excess > 0) {
             (bool success, ) = payable(msg.sender).call{value: excess}("");
             require(success, "Refund failed");
@@ -178,31 +178,14 @@ contract DecentralizedFinance is ERC20 {
         emit loanFinished(msg.sender, currentLoan.amount);
     }
 
-    function checkLoan2(uint256 loanID) external onlyOwner returns (string memory status, Loan memory) {
-        require(loans[loanID].borrower != address(0), "Loan does not exist");
-        
-        if(block.timestamp > nextPayment[loanID] && active[loanID]){
-            active[loanID] = false;
-            status = "Terminated - Collateral Lost";
-            Loan memory expiredLoan = loans[loanID];
-
-            return (status, expiredLoan);
-        }
-        else if(active[loanID]){
-            status = "Active";
-            return (status, loans[loanID]);
-        }
-        else{
-            status = "Terminated";
-            return (status, loans[loanID]);
-        }
-    }
-
+    // state of loan is considered as a status message, the deadline of the loan and the amount of cycles that have been payed
+    // status can be Terminated - Collateral Lost, Terminated or Active
     function checkLoan(uint256 loanID) external onlyOwner returns (string memory status,uint256 deadline,uint256 cycles){
         require(loans[loanID].borrower != address(0), "Loan does not exist");
 
-        if (block.timestamp > nextPayment[loanID] && active[loanID]) {
-            active[loanID] = false;
+        if (block.timestamp > nextPayment[loanID] && active[loanID]) { // when loan is not payed on time
+            // this ensures the collateral can't be recovered since if active == false no other loan operations can occur
+            active[loanID] = false; 
             status = "Terminated - Collateral Lost";
         } else if (active[loanID]) {
             status = "Active";
@@ -214,11 +197,11 @@ contract DecentralizedFinance is ERC20 {
         cycles = cyclesPaid[loanID];
     }
 
-    function getBalance() external view onlyOwner returns (uint256) {
+    function getBalance() external view onlyOwner returns (uint256) { // returns balance of contract in wei
         return balance;
     }
 
-    function getDexBalance() external view returns (uint256) {
+    function getDexBalance() external view returns (uint256) { // returns dex balance of user
         return balanceOf(msg.sender);
     }  
 }
